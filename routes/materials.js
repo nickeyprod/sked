008b9b6af3b029fec15843536e6f53247974ab6e
nickeyprod <nickeyprod@gmail.com>
2021-02-27 14:55:15 +0300

    Fix css/Add lower doors

--- routes/materials.js
+++ routes/materials.js
@@ -22,5 +22,10 @@ router.get("/prv-schema", function(req, res, next) {
   return res.render("materials/prv-schema", {title: "Расположение ПРВ"});
 });
 
+// GET /materials/lower-doors-lock
+router.get("/lower-doors-lock", function(req, res, next) {
+  return res.render("materials/lower-doors", {title: "Блокировка дверей трюма"});
+});
+
 
 module.exports = router;
