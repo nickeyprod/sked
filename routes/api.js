421fde863f3156a3d9dbdebff64c520484c12258
= <=>
2022-02-18 22:53:34 +0300

    Added api route for current month events from google cals

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,8 @@ const
   router = express.Router(),
   Performance = require("../models/performance");
 
+const ical = require('node-ical');
+
 // GET api/get-perfs-statistic
 router.get("/get-perfs-statistic", function (req, res, next) {
   Performance.aggregate([{ $group: { _id: "$type", count: { $sum: 1 } } }])
@@ -37,6 +39,30 @@ router.get("/auth-status", function (req, res, next) {
   return res.send({ 
     authorised: req.session.userId ? true : false,
     admin: req.session.admin === true ? true : false 
+  });
+});
+
+router.get("/ical", function(req, res, next) {
+  const calURL = 'https://calendar.google.com/calendar/ical/cjnp0knhgufvq3q7hvs1343s1k@group.calendar.google.com/public/basic.ics';
+
+  ical.async.fromURL(calURL, {}, (err, data) => { 
+    if (err) {
+      console.error(err);
+      return {error: "Error requesting or parsing calendar" };
+    }
+    const today = new Date(Date.now());
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const currMonthsEvents = [];
+
+    for (let key in data) {
+      const calDataStart = data[key].start;
+
+      if (calDataStart.getFullYear() == year && calDataStart.getMonth() == month) {
+        currMonthsEvents.push(data[key]);
+      }
+    }
+    return res.send({evts: currMonthsEvents });
   });
 });
 
