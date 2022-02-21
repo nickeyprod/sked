52a8808c821900bb385a64b36c3dc9144380577b
= <=>
2022-02-21 18:07:24 +0300

    Add scrolling to date

---
+++ public/js/repertuar.js
@@ -0,0 +1,25 @@
+class Repertuar {
+  constructor() {
+    this.rollLine = document.getElementById("event-container");
+    this.todaySquare = document.getElementById(`${new Date(Date.now()).getMonth()}-${new Date(Date.now()).getDate()}`);
+    this.target = this.todaySquare.offsetLeft - 200;
+
+    window.onload = () => {
+      this.startAnimatingScroll();
+      // this.rollLine.scrollLeft = this.todaySquare.offsetLeft - 200;
+    };
+  }
+
+  startAnimatingScroll() {
+    let speed = 10;
+    let d = 1;
+    let id = setInterval(() => {
+      if (this.rollLine.scrollLeft < this.target) {
+        d++;
+        this.rollLine.scrollLeft += 500 + (speed*d);
+      } else {
+        clearInterval(id);
+      }
+    }, 50);
+  }
+}
