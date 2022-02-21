0aa16b0507654c0995344db342c7bfcddd5ce74a
= <=>
2022-02-21 19:23:36 +0300

    Add calendars to menu/ fix errors

--- public/js/repertuar.js
+++ public/js/repertuar.js
@@ -1,7 +1,19 @@
 class Repertuar {
+
+
   constructor() {
     this.rollLine = document.getElementById("event-container");
-    this.todaySquare = document.getElementById(`${new Date(Date.now()).getMonth()}-${new Date(Date.now()).getDate()}`);
+    this.day = 1000 * 60 * 60 * 24;
+    this.dt = new Date(Date.now());
+
+    for(let i=0; i < 5; i++) {
+      this.todaySquare = document.getElementById(`${new Date(Date.now()).getMonth()}-${this.dt.getDate()}`);
+      if (!this.todaySquare) {
+        this.dt = new Date(this.dt.getTime() + this.day);
+      } else {
+        break
+      }
+    }
     this.target = this.todaySquare.offsetLeft - 200;
 
     window.onload = () => {
--- routes/calendars.js
+++ routes/calendars.js
@@ -38,4 +38,60 @@ router.get("/repertuar", function (req, res, next) {
   });
 });
 
+// GET /calendars/teh-jobs
+router.get("/teh-jobs", function (req, res, next) {
+  const calURL = 'https://calendar.google.com/calendar/ical/3visiklutvn26i0a4ug5is0ot0@group.calendar.google.com/public/basic.ics';
+
+  ical.async.fromURL(calURL, {}, (err, data) => {
+    if (err) {
+      console.error(err);
+      return { error: "Error requesting or parsing calendar" };
+    }
+
+    const todayDate = new Date(Date.now());
+    const year = todayDate.getFullYear();
+    const currYearEvents = [];    
+
+    for(let key in data) {
+      const evStart = new Date(data[key].start);
+      if (evStart.getFullYear() == year ) {
+        currYearEvents.push(data[key]);
+      }
+    }
+    currYearEvents.sort((firstEl, secondEl) => {
+      return firstEl.start - secondEl.start
+    });
+    return res.render("calendars/teh_events", { title: "Доп. Технические работы", events: currYearEvents });
+
+  });
+});
+
+// GET /calendars/load-jobs
+router.get("/load-jobs", function (req, res, next) {
+  const calURL = 'https://calendar.google.com/calendar/ical/4ll36qkg9m6dld63q95c3dgl2s@group.calendar.google.com/public/basic.ics';
+
+  ical.async.fromURL(calURL, {}, (err, data) => {
+    if (err) {
+      console.error(err);
+      return { error: "Error requesting or parsing calendar" };
+    }
+
+    const todayDate = new Date(Date.now());
+    const year = todayDate.getFullYear();
+    const currYearEvents = [];    
+
+    for(let key in data) {
+      const evStart = new Date(data[key].start);
+      if (evStart.getFullYear() == year ) {
+        currYearEvents.push(data[key]);
+      }
+    }
+    currYearEvents.sort((firstEl, secondEl) => {
+      return firstEl.start - secondEl.start
+    });
+    return res.render("calendars/load_events", { title: "График погрузок и работ с ЗГП", events: currYearEvents });
+
+  });
+});
+
 module.exports = router;
