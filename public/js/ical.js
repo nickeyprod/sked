3e083fdde7b032aae09e3f3b4cb271c945c71d54
= <=>
2022-02-22 17:35:00 +0300

    Fix day of the week order

--- public/js/ical.js
+++ public/js/ical.js
@@ -96,15 +96,14 @@ class iCal {
 
 
         // Detect next event
-        let hour = today.getTime() + 60000 * 60;
-        for (let b = 0; b < 4; b++) {
-            hour += 60000 * 60;
+        let hour = today.getTime();
+        for (let b = 0; b < 2; b++) {
+            hour +=  1000 * 60 * 60;     
             for(let i = 0; i < todayEvents.length; i++) {
                 const evStart = new Date(todayEvents[i].start).getTime();
-                const evEnd = new Date(evts[i].end).getTime();
+                const evEnd = new Date(todayEvents[i].end).getTime();
                 const currTime = today.getTime();
     
-                hour += 60000 * 60;
                 if (evStart < (currTime + hour) && evEnd > currTime) {
                     return todayEvents[i];
                 }
